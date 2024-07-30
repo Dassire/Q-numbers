7eb6c41fa39bff944a937bb2f2fa97af3cd69acb
Dassire <damienassire@gmail.com>
2024-07-30 17:08:46 +0200

    Changes to testRational

--- src/testRational.c
+++ src/testRational.c
@@ -23,6 +23,9 @@ int main(int argc, char const *argv[]) {
 	str=(x3 - x1 * x2).toString();
 	printf("%s\n", str);
 	delete[] str;
+	str=(x3 - x1 * x2).simplify().toString();
+	printf("%s\n", str);
+	delete[] str;
 	printf("%d\n", x2.cmp(x1));
 	return 0;
 }
