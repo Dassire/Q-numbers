ea30689217370f3e03d76178dcbff441826780d3
Dassire <damienassire@gmail.com>
2024-07-30 18:14:03 +0200

    Fix testEchelon2's memory leaks

--- src/testEchelon2.cpp
+++ src/testEchelon2.cpp
@@ -3,7 +3,8 @@
 #include <stdio.h>
 
 int main(int c, char** v) {
-	Matrix<Rational> M = create(4,4, new int[16] {1, 0, 4, 2, 1, 2, 6, 2, 2, 0, 8, 8, 2, 1, 10, 4});
+    int* tmp = new int[16] {1, 0, 4, 2, 1, 2, 6, 2, 2, 0, 8, 8, 2, 1, 10, 4};
+	Matrix<Rational> M = create(4,4, tmp);
 	Matrix<Rational> M3 = id(4);
 	Matrix<Rational> M2 = M.echelon(0);
 	printMatrix(M);
@@ -13,5 +14,6 @@ int main(int c, char** v) {
 	printMatrix(M3);
 	printf("\n");
 	printMatrix(M2 * M3);
+    delete[] tmp;
 	return 0;
 }
